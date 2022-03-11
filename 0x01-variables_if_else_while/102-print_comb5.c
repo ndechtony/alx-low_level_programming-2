1b583128f4ef445c4e32821761f6052812eae6b9
JoanKirimi <joannekirimi@gmail.com>
2022-03-11 07:10:36 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 
 /**
-  * main - Prints 3 combination of numbers
-  *
-  * Return: Always (Success)
-  */
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int c, i, k, j;
+int i, e, g, h, op1, op2;
 
-    for (c = 48; c <= 57; c++)
-    {
-        for (i = 48; i <= 57; i++)
-        {
-	    for (k = 48; k <= 57; k++)
-	    {
-	        for (j = 48; j <= 57; j++)
-	        {
-		    if (((k + j) > (c + i) &&  k >= c) || c < k)
-		    {
-		        putchar(c);
-		        putchar(i);
-		        putchar(' ');
-		        putchar(k);
-		        putchar(j);
-
-		    if (c + i + k + j == 227 && c == 57)
-		    {
-		    break;
-		    }
-		    else
-		    {
-		    putchar(',');
-		    putchar(' ');
-		    }
-		    }
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
+	{
+		e = 48;
+		while (e < 58)
+		{
+			i = 48;
+			while (i < 58)
+			{
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
+				{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			}
+			e++;
 		}
-	    }
+		g++;
 	}
-    }
-
-    putchar('\n');
-
-    return (0);
+	h++;
 }
-
+putchar('\n');
+return (0);
+}
