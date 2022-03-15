5294b22e5c2fb58a2471f51c0658bbcec127537a
JoanKirimi <joannekirimi@gmail.com>
2022-03-15 08:22:39 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
