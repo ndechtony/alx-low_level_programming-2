2c56f10b3540ca8987eb85064aa95014813aef52
JoanKirimi <joannekirimi@gmail.com>
2022-03-17 06:17:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9
+ * Description: You can only use _putchar twice
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
